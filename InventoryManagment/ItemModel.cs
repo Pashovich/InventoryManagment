651702ed5d0a9afd19c6c4210d327023d43a0f34
Pashovich <addons125@gmail.com>
2023-09-23 20:21:54 +1000

    Init Commit

---
+++ InventoryManagment/ItemModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagment
+{
+    internal class ItemModel
+    {
+        public int ItemID { get; set; }    
+        public string ItemName { get; set; }
+        public string Description { get; set; } 
+        public int Quantity { get; set; }   
+        public decimal Price { get; set; }   
+
+        
+        public ItemModel(int itemId, string itemName, string description, int quantity, decimal price)
+        {
+            ItemID = itemId;
+            ItemName = itemName;
+            Description = description;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        // Default constructor
+        public ItemModel()
+        {
+            
+        }
+
+    }
+}
